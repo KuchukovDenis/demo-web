e86d534c5db68913321ccdeaf0227e2c4fbe42da
denis <kuchukovdenis2003@mail.ru>
2023-02-17 14:04:33 +0500

    6.2

--- src/main/java/com/example/demoweb/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/com/example/demoweb/model/Post.java
@@ -1,5 +1,7 @@
 package com.example.demoweb.com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     public String getText() {
         return text;
@@ -11,8 +13,19 @@ public class Post {
 
     public Post(String text) {
         this.text = text;
+        creationDate = new Date();
     }
 
+    public Post(String text, Date creationDate) {
+        this.text = text;
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    private Date creationDate;
     private String text;
     private Integer likes;
 }
--- src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
@@ -4,13 +4,15 @@ import com.example.demoweb.com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class PostService {
     public ArrayList<Post> listAllPosts(){
 
+
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("post1"));
+        posts.add(new Post("post1" ));
         posts.add(new Post("post2"));
         posts.add(new Post("post3"));
         return posts;

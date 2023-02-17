752d6c728f7e8c15cd0bece6ad461a94f1585d69
denis <kuchukovdenis2003@mail.ru>
2023-02-17 14:39:24 +0500

    7.5

--- src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/com/example/demoweb/service/PostService.java
@@ -8,14 +8,14 @@ import java.util.Date;
 
 @Service
 public class PostService {
+    ArrayList<Post> posts = new ArrayList<>();
     public ArrayList<Post> listAllPosts(){
-
-
-        ArrayList<Post> posts = new ArrayList<>();
         posts.add(new Post("post1" ));
         posts.add(new Post("post2"));
         posts.add(new Post("post3"));
         return posts;
-
+    }
+    public void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
